a7e96fa6706f37e80a07096fd2189b57bf3275f3
Cyusa Munezero Keny <gasananike@gmail.com>
2022-03-24 15:41:30 +0200

    test1

--- server/src/__tests__/intergration/token.test.ts
+++ server/src/__tests__/intergration/token.test.ts
@@ -33,20 +33,4 @@ describe("Token API", () => {
 		});
 	});
 
-	// const { statusCode, body } = await request(`${serverURL}/api/tokens`, {
-	//         method: "POST",
-	//         headers: {
-	//           "Content-Type": "application/json",
-	//         },
-	//         body: JSON.stringify({
-	//           content: faker.lorem.paragraph(),
-	//           title: faker.lorem.sentence(),
-	//           authorId: 1,
-	//         }),
-	//       });
-
-	//       const respData = await body.json();
-
-	//       expect(statusCode).toBe(201);
-	//       expect(typeof respData).toBe("object");
 });
